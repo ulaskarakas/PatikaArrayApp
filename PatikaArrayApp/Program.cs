3663fc86b7ad8ed6f87622f48b8006c74ad3b905
ulaskarakas <ulaskarakas95@gmail.com>
2025-01-15 00:23:16 +0300

    Fix some bugs

--- PatikaArrayApp/Program.cs
+++ PatikaArrayApp/Program.cs
@@ -5,50 +5,36 @@
         int[] numbers = new int[10];
         for (int i = 0; i < 10; i++)
         {
-            Console.Write($"Lütfen {i+1}. sayıyı yazınız: ");
-            numbers[i] = int.Parse(Console.ReadLine());
+            Console.Write($"Please enter {i + 1}. number: ");
+            while (!int.TryParse(Console.ReadLine(), out numbers[i]))
+            {
+                Console.Write("Invalid value! Please enter a number: ");
+            }
         }
 
-        Console.WriteLine("Array'in elemanları:");
+        Console.WriteLine("Elements of array:");
         foreach (var item in numbers)
         {
             Console.WriteLine(item);
         }
 
         Array.Resize(ref numbers, numbers.Length + 1);
-        numbers[numbers.Length - 1] = 99;
 
-        //Array.Reverse(numbers);
-        Sorting(numbers);
+        Console.Write("Enter the number you want to add to array: ");
+        while (!int.TryParse(Console.ReadLine(), out numbers[numbers.Length - 1]))
+        {
+            Console.Write("Invalid value! Please enter a number: ");
+        }
 
-        Console.WriteLine("Büyükten küçüğe array'in elemanları: ");
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+
+        Console.WriteLine("Elements of array from largest to smallest: ");
         foreach (var item in numbers)
         {
             Console.WriteLine(item);
         }
 
-    }
-
-    static void Sorting(int[] array)
-    {
-        int n = array.Length;
-
-        // Tüm elemanları karşılaştır
-        for (int i = 0; i < n - 1; i++)
-        {
-            // Son sıralanmış elemanları dikkate alma
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                // Sıralı değilse yer değiştir
-                if (array[j] > array[j + 1])
-                {
-                    // Swap
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
     }
 
 }
